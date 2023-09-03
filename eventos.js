99069f0b135154e2169df9674f0dbc9e45d0ad8b
Santiamaro <santiagofederico090@gmail.com>
2023-09-03 18:15:38 -0300

    este commit lo hice porque al momento de darle click al boton saludar me saltaba dos veces la alerta con "hola", estrictamente me vi forzado a modificar el html y quitar la función onclick del boton debido a que todos los eventos tienen que pasar por el archivo js.

--- eventos.js
+++ eventos.js
@@ -1,7 +1,13 @@
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded",() => {
     const soydiv = document.getElementById("soydiv");
+    const boton = document.querySelector("button");
 
-    soydiv.addEventListener("click", ()=>  {
+    soydiv.addEventListener("click",() => {
         alert("Hola! Soy el div");
+    });
+
+    boton.addEventListener("click", (event) => {
+        event.stopPropagation(); // se usa stopPorpagation para detener al evento en ese mismo momento, de esta forma no intercede el div al darle click al boton saludar
+        alert("Hola!");
     });
 });
